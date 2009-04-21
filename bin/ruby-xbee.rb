aed0c50938e9d0d078ec96ba79f214dcdfe268bd
Mike Ashmore <mike@motomike.net>
2009-04-20 23:06:07 -0700

    Added, then commented out, some debugging code. w00t.

--- bin/ruby-xbee.rb
+++ bin/ruby-xbee.rb
@@ -1,12 +1,13 @@
 begin
   gem 'ruby-xbee'
 rescue LoadError => e
-  puts "LoadError?! => #{e}"
+  # puts "LoadError?! => #{e}"
   if require 'rubygems'
-    puts "Okay, required rubygems. retrying now ..."
+    # puts "Okay, required rubygems. retrying now ..."
     retry
   else
     $: << File.dirname(File.dirname(__FILE__)) + "/lib"
+    # puts "$: = #{$:.join(", ")}"
     require 'ruby_xbee'
   end
 end
